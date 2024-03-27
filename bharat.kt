8ecfcc7924ed747e3737c57f85e9368f5639069a
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-27 11:56:46 +0530

    learned oops implementation on kotlin

--- bharat.kt
+++ bharat.kt
@@ -1,3 +1,37 @@
+import java.awt.Shape
+
+class student(){
+   private lateinit var name : String
+   private var age : Int = 0
+
+    constructor(name: String, age: Int) : this() {
+        this.name = name
+        this.age = age
+    }
+}
+interface  shape{
+    fun area(): Double
+}
+class Rectangle (val width : Double , val height : Double) : shape{
+    override fun area(): Double {
+        return width*width
+    }
+
+}
+class Marker{
+    private lateinit var name : String
+    public lateinit var color : String
+    public var year : Int = 0
+    public var price : Int = 0
+    constructor(name: String , color :String , year : Int , price : Int){
+        this.name = name
+        this.year = year
+        this.price = price
+        this.color = color
+    }
+}
 fun main (){
-    println("what is your name tell me....")
+//    var  bharat = student("bharat ruidas " , 10)
+    var bluemarker = Marker("camel" , "black" , 2002 , 40)
+    println(bluemarker.year)
 }
